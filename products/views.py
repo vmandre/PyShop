c74b170992c9637e0e30331b5ba6234af5ac6398
Vanderlei Matos Andre <vmandre@gmail.com>
2025-04-09 23:32:27 -0300

    Add new product route

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the products index")
+
+
+def new(request):
+    return HttpResponse("New product")
